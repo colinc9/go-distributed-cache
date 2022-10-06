de745285e14c541deac63fb58590b64840ae637c
Colin Cai <colincai09@gmail.com>
2022-10-06 01:58:55 -0700

    fix: fix viper setup

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -10,8 +10,9 @@ var once sync.Once
 
 func GetDefaultInsCfg() *Config {
 	once.Do(func() {
+		SetUpViperDefault()
 		ins = &Config{
-			AppAddress:    viper.GetString("app.cache.port"),
+			AppAddress:    viper.GetString("app.cache.address"),
 		}
 	})
 	return ins
@@ -19,4 +20,14 @@ func GetDefaultInsCfg() *Config {
 
 type Config struct {
 	AppAddress    string
+}
+
+func SetUpViperDefault() {
+	viper.SetConfigName("app") // name of config file (without extension)
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		print(err)
+	}
 }
