4a7f479051416afd1bca356621f179770d1bb2a1
Colin Cai <colincai09@gmail.com>
2022-10-29 19:30:07 -0700

    fix: Change order of init execution

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	cache "github.com/colinc9/go-distributed-cache/pkg"
-	"github.com/colinc9/go-distributed-cache/pkg/network"
 )
 
 func main() {
 	cache.SetUpAndRun()
-	network.PeriodicSdkDiscovery()
 }
--- pkg/app.go
+++ pkg/app.go
@@ -3,9 +3,11 @@ package pkg
 import (
 	"github.com/colinc9/go-distributed-cache/pkg/config"
 	"github.com/colinc9/go-distributed-cache/pkg/controller"
+	"github.com/colinc9/go-distributed-cache/pkg/network"
 )
 
 func SetUpAndRun() {
 	config.GetDefaultInsCfg()
+	network.PeriodicSdkDiscovery()
 	controller.Run()
 }
